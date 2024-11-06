26d0033e14a9e965e2fc1fd8541965b6ac380c4b
renzomacchi <r.macchi.laamistad@gmail.com>
2024-11-06 19:05:56 -0300

    Registros 	-added registros editoriales 	-added registros generos 	-Observer incluido 	-Validate

--- P2Obligatorio2/src/Interfaz/Validate.java
+++ P2Obligatorio2/src/Interfaz/Validate.java
@@ -4,11 +4,20 @@
 */
 package Interfaz;
 
-public class Validate {
-    //Una clase que hace checks de diferentes a componentes que requieren inputs
+/**
+ * Una clase que hace checks de validacion a
+ * diferentes componentes que requieren inputs del usuario
+ * Tambien contiene mensajes de feedback en variables estaticas.
+ */
+public abstract class Validate {
 
     public final static String TXT_VACIO = "Debe llenar el formulario";
     
+    /**
+     * Dado un array de string, checkea que cada string no este vacio.
+     * @param campos
+     * @return 
+     */
     public static boolean sonTxtVacios(String[] campos) {
         boolean vacios = true;
         for(int i = 0; i < campos.length && vacios; i++) {
@@ -17,7 +26,12 @@ public class Validate {
         return vacios;
     }
     
+    /**
+     * Dado un string devuelve si esta vacio (o con espacios)
+     * @param campo
+     * @return 
+     */
     public static boolean esTxtVacio(String campo) {
-        return campo.equals("");
+        return campo.trim().equals("");
     }
 }
